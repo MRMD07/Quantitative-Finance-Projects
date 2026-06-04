fa7bba10ca381b0e5ccf86b513dfec789516c2a9
Muhammad Dayyan Tipu <dayyantipu@gmail.com>
2026-06-04 19:58:29 +0500

    Update analysis.py

--- KSE-100 Analysis/analysis.py
+++ KSE-100 Analysis/analysis.py
@@ -9,3 +9,14 @@ data['Price'] = data['Price'].str.replace(',','',regex = False).astype(float)
 data['Date'] = pd.to_datetime(data['Date'])
 data.sort_values(by = 'Date', ascending= True, inplace = True)
 data.reset_index(drop = True, inplace = True)
+
+data['Rolling_mean'] = data['Price'].rolling(window = 5).mean()
+
+date = np.array(data['Date'])
+price = np.array(data['Price'])
+
+change =(( price[1:]-price[:-1])/price[:-1])*100
+
+start_window = pd.to_datetime('2026-04-06')
+end_window = pd.to_datetime('2026-04-17')
+window =( data['Date']>= start_window)&( data['Date']<= end_window)
